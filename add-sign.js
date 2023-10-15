a0f134999ca3594fa24df4bcf576289634c6cbbd
DatSwede <88003385+DatSwede@users.noreply.github.com>
2023-10-14 19:08:04 -0700

    Update add-sign.js

--- add-sign.js
+++ add-sign.js
@@ -1,13 +1,11 @@
-document.getElementById('handle').addEventListener('input', function(event) {
-    let value = event.target.value;
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('handle').addEventListener('input', function(event) {
+        let value = event.target.value;
 
-    // If the value doesn't start with "@", and there's some input, add it
-    if (value.length > 0 && value[0] !== '@') {
-        event.target.value = '@' + value;
-    }
-    
-    // If the input is just the "@", remove it
-    else if (value === '@') {
-        event.target.value = '';
-    }
+        if (value.length > 0 && value[0] !== '@') {
+            event.target.value = '@' + value;
+        } else if (value === '@') {
+            event.target.value = '';
+        }
+    });
 });
